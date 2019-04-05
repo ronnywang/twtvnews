68a520ac219d2a3a8bf882a3f6db5bb9507613f5
Ronny Wang <ronnywang@gmail.com>
2019-04-05 15:26:56 +0800

    加入三立新聞，並略掉時間過長的影片

--- get-youtube-video.php
+++ get-youtube-video.php
@@ -2,6 +2,7 @@
 
 $channels = array(
     'ctitv' => 'https://www.youtube.com/channel/UCpu3bemTQwAU8PqM4kJdoEQ/videos',
+    'set' => 'https://www.youtube.com/user/setnews159/videos',
 );
 
 $crawl_video = function($channel, $date, $video_id, $title){
@@ -42,25 +43,29 @@ if (file_exists('youtube.csv')) {
 $output = fopen('youtube.csv', 'w');
 $columns = array('頻道', '日期', '影片ID', '標題', '影片長度', '觀看人數', '網址');
 fputcsv($output, $columns);
-foreach ($channels as $channel => $data) {
+foreach ($channels as $channel => $url) {
     $dir = __DIR__ . '/youtubes/' . $channel;
     if (!file_exists($dir)) {
         mkdir($dir);
     }
 
-    $content = file_get_contents('https://www.youtube.com/channel/UCpu3bemTQwAU8PqM4kJdoEQ/videos');
+    $content = file_get_contents($url);
     $content = str_replace('<head>', '<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">', $content);
-    for ($page  = 0; $page < 5; $page ++) {
+    for ($page  = 0; $page < 30; $page ++) {
         $doc = new DOMDocument;
 
         $doc->loadHTML($content);
         foreach ($doc->getElementsByTagName('a') as $a_dom) {
-            if (!preg_match('#^([0-9]+)中天新聞　*(.*)#u', $a_dom->nodeValue, $matches)) {
+            if (preg_match('#^([0-9]+)中天新聞　*(.*)#u', $a_dom->nodeValue, $matches)) {
+                $date = $matches[1];
+                $title = $matches[2];
+            } elseif (preg_match('#(.*[^0-9])([0-9]+)｜三立新聞台$#u', $a_dom->nodeValue, $matches)) {
+                $date = $matches[2];
+                $title = $matches[1];
+            } else {
                 continue;
             }
 
-            $date = $matches[1];
-            $title = $matches[2];
 
             if (!preg_match('#/watch\?v=([^&]*)$#', $a_dom->getAttribute('href'), $matches)) {
                 continue;
@@ -80,17 +85,24 @@ foreach ($channels as $channel => $data) {
                 }
             }
             $video_id = $matches[1];
-            $crawl_video($channel, $date, $video_id, $title);
             if (preg_match('#^- Duration: (\d+) seconds?.$#', trim($duration), $matches)) {
                 $duration = $matches[1];
             } else if (preg_match('#^- Duration: (\d+) minutes?, (\d+) seconds?.$#', trim($duration), $matches)) {
                 $duration = $matches[1] * 60 + $matches[2];
             } else if (preg_match('#^- Duration: (\d+) minutes?.$#', trim($duration), $matches)) {
                 $duration = $matches[1] * 60;
+            } else if (strpos($duration, 'hour')) {
+                continue;
             } else {
                 var_dump($duration);
                 throw new Exception($duration);
             }
+            if ($duration > 1000) {
+                continue;
+            }
+
+            $crawl_video($channel, $date, $video_id, $title);
+
             $view = str_replace(',', '', trim($view));
             $view = str_replace(' views', '', trim($view));
             $youtube_videos[$video_id] = array_combine(
