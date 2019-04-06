1f0871ebe98930fbd578d0ad80a37439b35eb30f
Ronny Wang <ronnywang@gmail.com>
2019-04-06 17:22:06 +0800

    增加可以輸入標題，並自動從 youtube 抓的功能

--- report.js
+++ report.js
@@ -1,3 +1,5 @@
+var videos = {};
+
 $(function(){
         // 先把前兩張圖都打開
         var loadmore = function(){
@@ -11,14 +13,119 @@ $(function(){
             });
         };
 
+        $('tbody').on('click', '.youtube-video', function(e){
+            e.preventDefault();
 
-        var videos = {};
+            var edit_tr_dom = $(this).parents('tr');
+            var record = $(this).data('youtube-record');
+            $('input[name="title"]', edit_tr_dom).val(record['標題'] + '(' + record['影片ID'] + ')');
+        });
+
+        $('tbody').on('keyup', 'input[name="title"]', function(e){
+                var text = $(this).val();
+                var records = youtube_records.filter(function(r) {
+                    if (match_date.indexOf(r['日期']) < 0) {
+                        return false;
+                    }
+                    return (r['標題'].indexOf(text) >= 0);
+                });
+                var edit_tr_dom = $(this).parents('tr');
+                $('.dropdown-menu', edit_tr_dom).html('');
+                records.map(function(r){
+                    var a_dom = $('<a></a>');
+                    a_dom.addClass('dropdown-item');
+                    a_dom.addClass('youtube-video');
+                    a_dom.data('youtube-record', r);
+                    a_dom.text(r['標題']);
+                    $('.dropdown-menu', edit_tr_dom).append(a_dom);
+                });
+                $(this).dropdown();
+        });
+
+        $('tbody').on('submit', '.group-form', function(e){
+            e.preventDefault();
+            var edit_tr_dom = $(this).parents('tr');
+            var tr_dom = edit_tr_dom.prev();
+
+            var title = $('input[name="title"]', edit_tr_dom).val();
+            if (tr_dom.data('title') != title) {
+                var action = ['set-title', tr_dom.data('start'), title];
+                action_logs.push(action);
+                do_action(action);
+                $('textarea').val(JSON.stringify(action_logs));
+            }
+            edit_tr_dom.remove();
+        });
+
+        $('tbody').on('click', 'tr.section', function(e){
+            e.preventDefault();
+            var tr_dom = $(this);
+            var edit_tr_dom = $('<tr></tr>');
+            var data = tr_dom.data('members');
+            var td_dom = $('<td></td>').attr('colspan', 5);
+
+            var id = 'input-' + (new Date()).getTime();
+            var form_dom = $('<form class="group-form dropdown">'
+                    + '標題:<input type="text" name="title" size="80" class="dropdown-toggle" data-toggle="dropdown" id="' + id + '">'
+                    + '<div class="dropdown-menu" aria-labelledby="' + id + '"></div>'
+                    + '<button type="submit">修改</button></form>'
+                    );
+            $('input[name="title"]', form_dom).val(tr_dom.data('title'));
+
+            td_dom.append(form_dom);
+
+            data.map(function(i){
+                var div_dom = $('<div></div>');
+                var record = videos[i];
+                div_dom.append($('<div></div>').append($('<img>').attr('src', record['img-file'])).append($('<img>').attr('src', record['crop-file'])));
+                td_dom.append(div_dom);
+            });
+            edit_tr_dom.append(td_dom);
+            edit_tr_dom.insertAfter(tr_dom);
+        });
+
+        var youtube_records = [];
+        var youtube_map = {};
+        var match_date = [];
+
+        $.get('https://raw.githubusercontent.com/ronnywang/twtvnews/master/youtube.csv', function(content){
+            youtube_records = $.csv.toObjects(content);
+            youtube_records = youtube_records.map(function(a, idx) {
+                a['images'] = [];
+                youtube_map[a['影片ID']] = idx;
+                return a;
+            });
+
+            var matches = document.location.href.match('([^/]*)/([0-9]{10,10})');
+            if (matches) {
+                var channel = matches[1];
+                var time = matches[2];
+                var date = new Date(time.substr(0, 4) + '/' + time.substr(4, 2) + '/' + time.substr(6, 2));
+                for (var i = 0; i < 2; i ++) {
+                    var yyyymmdd = date.getFullYear() + ('00' + (1 + date.getMonth())).substr(-2) + ('00' + date.getDate()).substr(-2);
+
+                    match_date.push(yyyymmdd);
+
+                    $.get('https://raw.githubusercontent.com/ronnywang/twtvnews-youtube-titles/gh-pages/' + channel + '/' + yyyymmdd + '.csv', function(content){
+                        var terms = $.csv.toArrays(content);
+                        terms.map(function(term){
+                            if (youtube_records[youtube_map[term[1]]]) {
+                                youtube_records[youtube_map[term[1]]]['images'].push(parseInt(term[3]));
+                            }
+                        });
+                    }, 'text');
+
+                    date.setTime(date.getTime() - 86400 * 1000);
+                }
+            }
+        }, 'text');
+
         $.get('data.json', function(records){
                 for (var i = 0; i < records.length; i ++) {
                     var record = records[i];
                     var div_dom = $('<div></div>').addClass('group');
 
-                    videos[record.start] = record;
+                    videos[parseInt(record.start)] = record;
                     div_dom.data('start', record.start);
                     div_dom.data('end', record.end);
                     div_dom.data('youtube-id', record['youtube-id']);
@@ -51,35 +158,49 @@ $(function(){
             var end = parseInt(group_dom.data('end'));
             var youtube_id = group_dom.data('youtube-id');
             var youtube_title = group_dom.data('youtube-title');
+            var action_params = [];
+
+            if (typeof(action) === 'object') {
+                action_params = action.slice(1);
+                action = action[0];
+            }
 
             if (action == 'btn-right') { // 正確
-                var tr_dom = $('<tr></tr>');
+                var tr_dom = $('<tr></tr>').addClass('section');
+                tr_dom.attr('id', 'group-' + start);
                 tr_dom.data('start', start)
                     .data('end', end)
                     .data('youtube-id', youtube_id)
                     .data('youtube-title', youtube_title)
                     .data('type', 'news')
-                    .data('members', [])
+                    .data('members', [start])
+                    .data('title', youtube_title + '(' + youtube_id + ')')
                 ;
                 tr_dom.append($('<td></td>').text(get_time_string(start)));
                 tr_dom.append($('<td></td>').text(get_time_string(end)));
                 tr_dom.append($('<td></td>').text(end - start + 1));
                 tr_dom.append($('<td></td>').text('新聞段落'));
-                tr_dom.append($('<td></td>').text(youtube_id + ':' + youtube_title));
+                tr_dom.append($('<td></td>').text(youtube_title + '(' + youtube_id + ')'));
                 $('tbody').append(tr_dom);
             } else if (action == 'btn-like-prev') {
                 var last_tr_dom = $('tbody tr:last');
+                var members = last_tr_dom.data('members');
+                members.push(start);
+                last_tr_dom.data('members', members);
+
                 start = last_tr_dom.data('start');
                 last_tr_dom.data('end', end);
                 if (!last_tr_dom.data('youtube-id') && youtube_id) {
                     last_tr_dom.data('youtube-id', youtube_id);
                     last_tr_dom.data('youtube-title', youtube_title);
-                    $('td', last_tr_dom).eq(4).text(youtube_id + ':' + youtube_title);
+                    $('td', last_tr_dom).eq(4).text(youtube_title + '(' + youtube_id + ')');
+                    last_tr_dom.data('title', youtube_title + '(' + youtube_id + ')');
                 }
                 $('td', last_tr_dom).eq(1).text(get_time_string(end));
                 $('td', last_tr_dom).eq(2).text(end - start + 1);
             } else if (action == 'btn-ad') {
-                var tr_dom = $('<tr></tr>');
+                var tr_dom = $('<tr></tr>').addClass('section');
+                tr_dom.attr('id', 'group-' + start);
                 tr_dom.data('start', start)
                     .data('end', end)
                     .data('type', 'ad')
@@ -92,11 +213,12 @@ $(function(){
                 tr_dom.append($('<td></td>').text(''));
                 $('tbody').append(tr_dom);
             } else if (action == 'btn-section-nexton') {
-                var tr_dom = $('<tr></tr>');
+                var tr_dom = $('<tr></tr>').addClass('section');
+                tr_dom.attr('id', 'group-' + start);
                 tr_dom.data('start', start)
                     .data('end', end)
                     .data('type', 'section-nexton')
-                    .data('members', [])
+                    .data('members', [start])
                 ;
                 tr_dom.append($('<td></td>').text(get_time_string(start)));
                 tr_dom.append($('<td></td>').text(get_time_string(end)));
@@ -105,11 +227,12 @@ $(function(){
                 tr_dom.append($('<td></td>').text(''));
                 $('tbody').append(tr_dom);
             } else if (action == 'btn-section-start') {
-                var tr_dom = $('<tr></tr>');
+                var tr_dom = $('<tr></tr>').addClass('section');
+                tr_dom.attr('id', 'group-' + start);
                 tr_dom.data('start', start)
                     .data('end', end)
                     .data('type', 'section-start')
-                    .data('members', [])
+                    .data('members', [start])
                 ;
                 tr_dom.append($('<td></td>').text(get_time_string(start)));
                 tr_dom.append($('<td></td>').text(get_time_string(end)));
@@ -117,6 +240,13 @@ $(function(){
                 tr_dom.append($('<td></td>').text('開播畫面'));
                 tr_dom.append($('<td></td>').text(''));
                 $('tbody').append(tr_dom);
+            } else if (action == 'set-title') {
+                var start = action_params[0];
+                var title = action_params[1];
+                var tr_dom = $('tbody #group-' + start);
+                $('td', tr_dom).eq(4).text(title);
+                tr_dom.data('title', title);
+                return;
             } else {
                 alert('還沒實作這動作');
                 return;
